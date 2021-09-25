82ebc4171a146d34d6afbeb4beb051f148141ad7
thiago0309 <thiagovsl001@gmail.com>
2021-09-25 01:33:49 -0300

    Add opções

--- tabuada.js
+++ tabuada.js
@@ -5,6 +5,8 @@ function tabuada() {
         alert('Erro!! Digite um número')
     }
     else {
+        let escolha = document.getElementsByName('radesco')
+        if (escolha[0].checked) {
         var n = Number(numero.value)
         select.innerHTML = ""
         for (let c = 1; c <= 10; c++) {
@@ -12,5 +14,16 @@ function tabuada() {
             item.text = ` ${n} x ${c} = ${n*c}`
             select.appendChild(item)
         }
+        }
+        if (escolha[1].checked) {
+            var n = Number(numero.value)
+            select.innerHTML = ""
+            for (let c = 1; c <= 10; c++) {
+                let item = document.createElement('option')
+                item.text = ` ${n} ÷ ${c} = ${n/c}`
+                select.appendChild(item)
+            }
+        }
+        
     }
 }
